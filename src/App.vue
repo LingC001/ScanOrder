9faa9dba247ddd67597043a884a9b629f0d287b6
Alisa-Don <2427667633@qq.com>
2022-08-12 02:57:37 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <order-index />
+  </div>
+</template>
+
+<script>
+import orderIndex from '@/view/orderIndex/orderIndex'
+
+export default {
+  name: 'App',
+  components: {
+    orderIndex
+  }
+}
+</script>
+
+<style>
+
+</style>
