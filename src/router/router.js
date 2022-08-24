b5f2060f98f5f4731cfc0667d4e67621c17c79e5
bo yang <1070939301@qq.com>
2022-08-24 18:21:12 +0800

    refactor:文件名修改

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import OrderIndex from '@/views/orderIndex/orderIndex.vue'
+import OrderIndex from '@/views/orderIndex/OrderIndex.vue'
 import ChooseView from '@/views/choose/ChooseView.vue'
 import ShopDetail from '@/views/shopDetail/ShopDetail.vue'
 import SubmitOrder from '@/views/submitOrder/SubmitOrder.vue'
